e9e3391801a27f70bfb6a8ab503b6d44b56da99a
Shaghay <102187856+Shaghay@users.noreply.github.com>
2023-05-08 17:35:00 +0200

    nested routes

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Layout from './components/Layout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostLayout from './components/HostLayout'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetail from './pages/Host/HostVanDetail'
+
+import './index.css' 
+import "../server"
+import HostVanPrice from './pages/Host/HostVanPrice'
+import HostVanInfo from './pages/Host/HostVanInfo'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='vans' element={<Vans />} />
+          <Route path='vans/:id' element={<VanDetail />} />
+
+          <Route path='host' element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='income' element={<Income />} />
+            <Route path='reviews' element={<Reviews />} />
+            <Route path='vans' element={<HostVans />} />
+
+            <Route path='vans/:id' element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path='price' element={<HostVanPrice />} />
+              <Route path='photos' element={<HostVanPhotos />} />
+            </Route>
+          </Route> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<App />)
